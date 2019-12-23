1f5e96e8ce09c04fad69d144ce303d0037687d3c
JH <oh.ji-hoon@daum.net>
2019-12-23 16:56:55 +0900

    Ch10 Clear

---
+++ Tab/Tab/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by 오지훈 on 23/12/2019.
+//  Copyright © 2019 JiHoon Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+    
+}
+
