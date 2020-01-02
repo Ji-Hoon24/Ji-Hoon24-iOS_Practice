efaa5efa73171f5fe5ce34da74a1c02531f000ed
JH <oh.ji-hoon@daum.net>
2020-01-02 10:41:10 +0900

    Ch15 Clear

---
+++ DrawGraphics/DrawGraphics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawGraphics
+//
+//  Created by 오지훈 on 02/01/2020.
+//  Copyright © 2020 JiHoon Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
